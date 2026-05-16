f8063771e053b1b30c0af65393092bc717221ba1
hanyun <1355081829@qq.com>
2026-05-16 12:52:41 +0800

    2.0.0

--- templates/template-vue3-ts/rsbuild.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginVue } from '@rsbuild/plugin-vue';
-import { pluginNodePolyfill } from '@rsbuild/plugin-node-polyfill';
-import { electronRs } from 'electron-rs';
-
-// Docs: https://rsbuild.rs/config/
-export default defineConfig({
-  plugins: [pluginVue(), electronRs(), pluginNodePolyfill()],
-});
